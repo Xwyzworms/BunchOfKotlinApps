04df001e6c6e3b87e2bc4348868eed95c0e8c6ae
XwyzWorm <pratamaazmi1@gmail.com>
2020-12-31 16:27:59 +0700

    Udah kelar cuma konversi doang

---
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun ConvertTheInput(view :View)
+    {
+        if(AmountInput.text.isNotEmpty()){
+                val dollarValue = AmountInput.text.toString().toFloat()
+                val converted = dollarValue * 0.85
+
+            textView.text = converted.toString()
+        }else{
+            textView.text=getString(R.string.HelloApplication)
+        }
+
+
+
+    }
+}
