5cf7c56f489b418274f44bcb609aa63afdd57ad1
samsepi <pratamaazmi1@gmail.com>
2021-02-03 21:09:28 +0700

    Add more implementation of how to use SharedPreference

--- ListToTheEdge/app/src/main/java/com/example/listtotheedge/TaskList.kt
+++ ListToTheEdge/app/src/main/java/com/example/listtotheedge/TaskList.kt
@@ -1,4 +1,10 @@
 package com.example.listtotheedge
 
-class TaskList (val name :String, val tasks :ArrayList<String> = ArrayList()) {
+class TaskList (private var named :String, val tasks :ArrayList<String> = ArrayList()) {
+         var name : String = named
+            get() = field ?: "Ga Ada NAma"
+            set(value){
+                field = value
+            }
+
 }
