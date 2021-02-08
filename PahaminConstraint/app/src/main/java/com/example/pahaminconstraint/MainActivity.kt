30d759218ff88048a0a5c55d5e89cffc1ecc11da
XwyzWorm <pratamaazmi1@gmail.com>
2021-02-08 09:46:48 +0700

    dah sampe cara gunain rad button

--- PahaminConstraint/app/src/main/java/com/example/pahaminconstraint/MainActivity.kt
+++ PahaminConstraint/app/src/main/java/com/example/pahaminconstraint/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.pahaminconstraint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import org.w3c.dom.Text
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         val btnApply: Button = findViewById<Button>(R.id.btnApply)
         val btnTambah : Button = findViewById<Button>(R.id.btnTambah)
+        val btnAdd : Button = findViewById<Button>(R.id.btnAdd)
+        val btnGambar : Button = findViewById<Button>(R.id.btnGambar)
+        val btnOrder : Button = findViewById<Button>(R.id.btnOrder)
         var counter = 0
+
         btnApply.setOnClickListener{it ->
             // Initiate Variable
             val firstName : String = findViewById<EditText>(R.id.etFirstName).text.toString()
@@ -29,9 +33,39 @@ class MainActivity : AppCompatActivity() {
             counter += 1
             val theText : TextView = findViewById<TextView>(R.id.tvHitung)
             val default : String = getText(R.string.dahlah).toString()
-            theText.setText(default + counter)
+            theText.text = default + counter        }
+        btnAdd.setOnClickListener{it->
+            var num1 : Int = findViewById<EditText>(R.id.etFirstNum).text.toString().toInt()
+            var num2 : Int = findViewById<EditText>(R.id.etSecNum).text.toString().toInt()
+
+            var ans : Int = num1 + num2
+            val tvHasil : TextView = findViewById<TextView>(R.id.tvHasil)
+            Log.d("MainActivity","KEbentuk?")
+            tvHasil.text = ans.toString()
+
         }
 
+        btnGambar.setOnClickListener(View.OnClickListener(){
+          val tvImage :ImageView = findViewById(R.id.imvGithub)
+          tvImage.setImageResource(R.drawable.github)
+        })
+
+        btnOrder.setOnClickListener(View.OnClickListener {
+            val RadQuest : RadioGroup = findViewById<RadioGroup>(R.id.rgQuest)
+            val checkedRadGroup : Int = RadQuest.checkedRadioButtonId
+            val RadButton : RadioButton = findViewById<RadioButton>(checkedRadGroup)
+            val isGojChecked : Boolean = findViewById<CheckBox>(R.id.cbgojay).isChecked
+            val isSimChecked : Boolean = findViewById<CheckBox>(R.id.cbsimon).isChecked
+            val isGratisChecked: Boolean = findViewById<CheckBox>(R.id.cbGratis).isChecked
+
+            val OrderedString : String = "Hello today you order :\n"+
+                    "${RadButton.text}\n" +
+                    (if (isGojChecked) "Gojay\n" else "")+
+                    (if(isSimChecked)"Simon Viklund\n" else "")+
+                    (if(isGratisChecked) "Gratis Boy\n" else "")
+            val TvOrder : TextView = findViewById<TextView>(R.id.tvListPesanan)
+            TvOrder.text = OrderedString
+        })
 
 
     }
